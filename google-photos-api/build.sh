10474c03400dc4977815c31af02dc9e5f482d562
singularis <singularis314@gmail.com>
2023-09-26 16:04:58 +0000

    Fixes doekcer configs

--- google-photos-api/build.sh
+++ google-photos-api/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t singularis314/gphoto_downloader:0.2 .
-docker push singularis314/gphoto_downloader:0.2
-kubectl delete -f gphoto.yaml
+docker build -t singularis314/gphoto_downloader:0.3 .
+docker push singularis314/gphoto_downloader:0.3
+kubectl delete -f gphoto.yaml --force
 kubectl apply -f gphoto.yaml

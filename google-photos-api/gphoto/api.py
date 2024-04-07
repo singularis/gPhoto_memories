a36361cb536b89767f7420f247619573096556e8
singularis <singularis314@gmail.com>
2024-04-07 09:43:05 +0000

    Api token expritation handling

--- google-photos-api/gphoto/api.py
+++ google-photos-api/gphoto/api.py
@@ -3,6 +3,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 import pickle
 import os
 import logging
+import google.auth.exceptions
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +23,8 @@ class GooglePhotosApi:
             scopes = ['https://www.googleapis.com/auth/photoslibrary']
         self.api_name = api_name
         self.account_name = account_name
-        self.client_secret_file = os.path.join(client_secret_path + f"gcp-credentials-{account_name}" + f"/gPhoto_credentials_{account_name}" + ".json")
+        self.client_secret_file = os.path.join(client_secret_path + f"gcp-credentials-{account_name}"
+                                               + f"/gPhoto_credentials_{account_name}" + ".json")
         self.api_version = api_version
         self.scopes = scopes
         self.cred_pickle_file = f'/app/credentials/token_{self.account_name}_{self.api_name}_{self.api_version}.pickle'
@@ -34,12 +37,20 @@ class GooglePhotosApi:
 
         if not self.cred or not self.cred.valid:
             if self.cred and self.cred.expired and self.cred.refresh_token:
-                self.cred.refresh(Request())
-            else:
+                try:
+                    self.cred.refresh(Request())
+                except google.auth.exceptions.RefreshError as e:
+                    logger.critical(f"Cannot refresh creds, {e}, cleaning up existing token")
+                    os.unlink(self.cred_pickle_file)
+                    logger.info(f"Cleaned up configs, starting clean flow")
+            try:
                 flow = InstalledAppFlow.from_client_secrets_file(self.client_secret_file, self.scopes)
                 self.cred = flow.run_local_server()
 
-            with open(self.cred_pickle_file, 'wb') as token:
-                pickle.dump(self.cred, token)
+                with open(self.cred_pickle_file, 'wb') as token:
+                    pickle.dump(self.cred, token)
+            except Exception as e:
+                logger.critical(f"Unknown except {e}, exiting")
+                exit()
 
         return self.cred
